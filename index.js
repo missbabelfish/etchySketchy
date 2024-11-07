624efb6c0de9664a8fc9c569a965b91d94b5b1e2
missbabelfish <missbabelfish@gmail.com>
2024-11-07 16:14:15 -0600

    initial commit, basic functionality working. need to clear squares before drawing new ones

---
+++ index.js
@@ -0,0 +1,26 @@
+const holder = document.querySelector('.containerHolder')
+const container = document.querySelector('.container')
+const chooseSquaresButton = document.querySelector('#chooseSquares')
+const squaresDialog = document.querySelector('#squaresDialog')
+const squaresInput = document.querySelector('#squares')
+const squaresSubmit = document.querySelector('#submitSquares')
+squaresDialog.returnValue = null
+
+chooseSquaresButton.addEventListener('click', () => squaresDialog.showModal())
+squaresSubmit.addEventListener('click', chooseSquares)
+
+
+function drawSquares(squares) {
+    for (let i = 0; i < squares**2; i++) {
+        const square = document.createElement('div')
+        square.style.width = `${100/squares}%`
+        square.style.height = `${100/squares}%`
+        container.appendChild(square)
+    }
+    
+}
+
+function chooseSquares() {
+    const squaresChosen = squaresInput.value
+    drawSquares(squaresChosen)
+}
